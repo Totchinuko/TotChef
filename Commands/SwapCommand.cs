27af8e4eb8948560d3dfd3d5df48bb0ba6425a86
Totchinuko <contact@totchinuko.fr>
2023-08-30 11:42:18 +0200

    Fix include cook file not working

--- Commands/SwapCommand.cs
+++ Commands/SwapCommand.cs
@@ -11,12 +11,14 @@ namespace Tot.Commands
     [Verb("swap", HelpText = "Swap files in the cookinfo.ini")]
     internal class SwapCommand : ModBasedCommand, ICommand
     {
-        [Option('f', "filter", HelpText = "Folder filtering, accept * wildcards on file name")]
-        public string? filter { get; set; }
-        [Option('r', "recursive", HelpText = "Include files from subfolder")]
-        public bool recursive { get; set; }
         [Option('e', "exclude", HelpText = "Swap files to the exclude list")]
         public bool exclude { get; set; }
+
+        [Option('f', "filter", HelpText = "Folder filtering, accept * wildcards on file name")]
+        public string? filter { get; set; }
+
+        [Option('r', "recursive", HelpText = "Include files from subfolder")]
+        public bool recursive { get; set; }
 
         public CommandCode Execute()
         {
@@ -29,24 +31,28 @@ namespace Tot.Commands
             if (!string.IsNullOrEmpty(filter) && !filter.PosixFullName().StartsWith(clerk.DevKitContent.PosixFullName()))
                 filter = Path.Join(clerk.DevKitContent.FullName, filter);
 
-            List<string> from;
-            List<string> to;
-            if(exclude)
-                clerk.GetCookInfo(out from, out to);
-            else
-                clerk.GetCookInfo(out to, out from);
+            List<string> included;
+            List<string> excluded;
+            clerk.GetCookInfo(out included, out excluded);
 
             List<string> added = new List<string>();
 
             if (File.Exists(filter))
             {
-                added = clerk.SwapFilesInLists(new List<string> { filter.PosixFullName() }, ref to, ref from);
+                if (exclude)
+                    added = clerk.SwapFilesInLists(new List<string> { filter.PosixFullName() }, ref excluded, ref included);
+                else
+                    added = clerk.SwapFilesInLists(new List<string> { filter.PosixFullName() }, ref included, ref excluded);
             }
             else if (Directory.Exists(filter))
             {
                 DirectoryInfo directory = new DirectoryInfo(filter).GetProperCasedDirectoryInfo();
                 List<string> files = Directory.GetFiles(directory.FullName, $"*.{KitchenClerk.AssetExt}", recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly).ToList();
-                added = clerk.SwapFilesInLists(files, ref to, ref from);
+
+                if (exclude)
+                    added = clerk.SwapFilesInLists(files, ref excluded, ref included);
+                else
+                    added = clerk.SwapFilesInLists(files, ref included, ref excluded);
             }
             else if (filter.Contains("*"))
             {
@@ -55,14 +61,17 @@ namespace Tot.Commands
                     return CommandCode.NotFound(fileInfo);
 
                 List<string> files = Directory.GetFiles(fileInfo.Directory.FullName, filter.Substring(fileInfo.Directory.FullName.Length + 1), recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly).ToList();
-                added = clerk.SwapFilesInLists(files, ref to, ref from);
+                if (exclude)
+                    added = clerk.SwapFilesInLists(files, ref excluded, ref included);
+                else
+                    added = clerk.SwapFilesInLists(files, ref included, ref excluded);
             }
             else
                 return CommandCode.NotFound(new DirectoryInfo(filter));
 
-            if (!clerk.SetCookInfo(from, to))
+            if (!clerk.SetCookInfo(included, excluded))
                 return clerk.LastError;
-            clerk.DumpChange(added, ConsoleColor.Red);
+            clerk.DumpChange(added, exclude ? ConsoleColor.Red : ConsoleColor.Green);
             return CommandCode.Success($"{added.Count} files added to the {(exclude ? "exclude" : "include")} list");
         }
     }
