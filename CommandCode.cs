fa4a48a01a6d035ba5e11c76b3ec4a6c11977c41
Totchinuko <contact@totchinuko.fr>
2023-12-05 21:11:59 +0100

    New: New error shorcuts

--- CommandCode.cs
+++ CommandCode.cs
@@ -8,26 +8,35 @@ namespace Tot
 {
     public struct CommandCode
     {
-        public int code;
-        public string message;
-
-        public const int UnknownError = 1;
-        public const int MissingArgument = 10;
-        public const int FileNotFound = 100;
-        public const int FileLocked = 101;
-        public const int DirectoryNotFound = 102;
+        public const int CookingFailure = 204;
         public const int DevKitPathInvalid = 200;
+        public const int DirectoryNotFound = 102;
+        public const int FileLocked = 101;
+        public const int FileNotFound = 100;
+        public const int MissingArgument = 10;
         public const int ModNameIsInvalid = 201;
         public const int RepositoryIsDirty = 202;
         public const int RepositoryWrongBranch = 203;
-        public const int CookingFailure = 204;
+        public const int UnknownError = 1;
+        public int code;
+        public string message;
+
+        public static CommandCode Error(string message) => new CommandCode { code = UnknownError, message = message };
+
+        public static CommandCode Exception(Exception ex) => new CommandCode { code = UnknownError, message = ex.Message };
+
+        public static CommandCode Forbidden(FileInfo file) => new CommandCode { code = FileNotFound, message = $"File cannot be accessed: {file.FullName}" };
+
+        public static CommandCode MissingArg(string name) => new CommandCode { code = MissingArgument, message = $"Missing argument {name}" };
+
+        public static CommandCode NotFound(DirectoryInfo directory) => new CommandCode { code = DirectoryNotFound, message = $"Directory not found: {directory.FullName}" };
+
+        public static CommandCode NotFound(FileInfo file) => new CommandCode { code = FileNotFound, message = $"File not found: {file.FullName}" };
 
         public static CommandCode Success(string message) => new CommandCode { code = 0, message = message };
+
         public static CommandCode Success() => new CommandCode { code = 0, message = "" };
+
         public static CommandCode Unknown() => new CommandCode { code = 0, message = "Internal Error" };
-        public static CommandCode NotFound(DirectoryInfo directory) => new CommandCode { code = DirectoryNotFound, message = $"Directory not found: {directory.FullName}" };
-        public static CommandCode NotFound(FileInfo file) => new CommandCode { code = FileNotFound, message = $"File not found: {file.FullName}" };
-        public static CommandCode Forbidden(FileInfo file) => new CommandCode { code = FileNotFound, message = $"File cannot be accessed: {file.FullName}" };
-        public static CommandCode MissingArg(string name) => new CommandCode { code = MissingArgument, message = $"Missing argument {name}" };
     }
 }
