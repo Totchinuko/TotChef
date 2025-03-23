a1dfa6e48869c9d4a8e0122d61c16005aca12ecb
Totchinuko <contact@totchinuko.fr>
2025-03-23 11:09:44 +0100

    Fixed the modinfo class to output the same json as devkit

--- tot/ModinfoData.cs
+++ tot/ModinfoData.cs
@@ -9,32 +9,32 @@ namespace Tot
 {
     internal class ModinfoData
     {
-        [JsonPropertyName("author")]
-        public string Author { get; set; } = string.Empty;
-
-        [JsonPropertyName("bRequiresLoadOnStartup")]
-        public bool BRequiresLoadOnStartup { get; set; } = false;
-
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+        
+        [JsonPropertyName("description")]
+        public string Description { get; set; } = string.Empty;
+        
         [JsonPropertyName("changeNote")]
         public string ChangeNote { get; set; } = string.Empty;
 
-        [JsonPropertyName("description")]
-        public string Description { get; set; } = string.Empty;
+        [JsonPropertyName("author")]
+        public string Author { get; set; } = string.Empty;
+        
+        [JsonPropertyName("authorUrl")]
+        public string AuthorUrl { get; set; } = string.Empty;
 
-        [JsonPropertyName("fileSize")]
-        public int FileSize { get; set; } = -2;
+        [JsonPropertyName("versionMajor")]
+        public int VersionMajor { get; set; } = 0;
 
-        [JsonPropertyName("folderName")]
-        public string FolderName { get; set; } = string.Empty;
+        [JsonPropertyName("versionMinor")]
+        public int VersionMinor { get; set; } = 0;
 
-        [JsonPropertyName("name")]
-        public string Name { get; set; } = string.Empty;
+        [JsonPropertyName("versionBuild")]
+        public int VersionBuild { get; set; }
 
-        [JsonPropertyName("revisionNumber")]
-        public int RevisionNumber { get; set; } = 0;
-
-        [JsonPropertyName("snapshotId")]
-        public int SnapshotId { get; set; } = 0;
+        [JsonPropertyName("bRequiresLoadOnStartup")]
+        public bool BRequiresLoadOnStartup { get; set; } = false;
 
         [JsonPropertyName("steamPublishedFileId")]
         public string SteamPublishedFileId { get; set; } = string.Empty;
@@ -45,13 +45,16 @@ namespace Tot
         [JsonPropertyName("steamVisibility")]
         public int SteamVisibility { get; set; } = 0;
 
-        [JsonPropertyName("versionBuild")]
-        public int VersionBuild { get; set; }
+        [JsonPropertyName("folderName")]
+        public string FolderName { get; set; } = string.Empty;
 
-        [JsonPropertyName("versionMajor")]
-        public int VersionMajor { get; set; } = 0;
+        [JsonPropertyName("revisionNumber")]
+        public int RevisionNumber { get; set; } = 0;
 
-        [JsonPropertyName("versionMinor")]
-        public int VersionMinor { get; set; } = 0;
+        [JsonPropertyName("snapshotId")]
+        public int SnapshotId { get; set; } = 0;
+
+        [JsonPropertyName("fileSize")]
+        public int FileSize { get; set; } = -2;
     }
 }
